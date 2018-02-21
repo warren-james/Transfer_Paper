32769bf8ae5e9b461a8e165894c959e3f3a8e5ae
warren-james <w.james@abdn.ac.uk>
2018-02-21 14:44:49 +0000

    Datasets added
    Datasets in a format for Elle added

--- 6_New_datasets.R
+++ 6_New_datasets.R
@@ -14,6 +14,7 @@
 
 #### libraries needed ####
 library(tidyverse)
+library(reshape2)
 
 #### Extra information ####
 # I think there were 36 pixels per degree... Will have to check though
@@ -80,28 +81,33 @@ acc_sep <- tibble(
 )
 
 # loop through participants for accuracy over all separations
-for (p in unique(df$participant))
-{
-  # general linear model
-  ss = df[which(df$participant==p),]
-  m = glm(data=ss, accuracy~separation, family=binomial(mafc.probit(2)))
-  for(i in 1:640){
-    y = predict(m, data.frame(separation = i), type = "response")
-    
-    # add into new data frame
-    acc_sep <- rbind(acc_sep, data.frame(participant = p, 
-                                         separation  = i,
-                                         accuracy = y))
-  }
-}
+# very slow loop... must be a quicker way to do this?
+# the for(i in 1:640) causes it to slow... Can do this another way?
+#for (p in unique(df$participant))
+#{
+#  # general linear model
+#  ss = df[which(df$participant==p),]
+#  m = glm(data=ss, accuracy~separation, family=binomial(mafc.probit(2)))
+#  for(i in 1:640){
+#    y = predict(m, data.frame(separation = i), type = "response")
+#    
+#    # add into new data frame
+#    acc_sep <- rbind(acc_sep, data.frame(participant = p, 
+#                                         separation  = i,
+#                                         accuracy = y))
+#  }
+#}
 
 # participant 1 might need to be excluded
 
 # save this 
-save(acc_sep, file = "scratch/acc_sep")
+# save(acc_sep, file = "scratch/acc_sep")
+
+# load in the above
+load("scratch/acc_sep")
 
 # tidy 
-rm(m, ss, i, p, sep, y)
+# rm(m, ss, i, p, sep, y)
 
 #### Part 1 plots #### 
 # summray data for mean accuracy 
@@ -155,13 +161,23 @@ switch_df$switch_point <- round(switch_df$switch_point)
 #     might need to reorder the levels before you do this 
 #     Though there seem to be nine levels for each participant, not 7?
 
+#### Standardise separations ####
+for(i in unique(switch_df$participant)){
+  switch_df$standard_sep[switch_df$participant == i] <- as.numeric(as.factor(switch_df$separation[switch_df$participant == i]))
+}
 
-#### dataset for accuracy given fixation location ####
+# subtract 5 to centre the switch point 
+switch_df$standard_sep <- switch_df$standard_sep - 5
+
+#tidy
+rm(i)
+
+#### column for accuracy given fixation location ####
 # gives accuracy given where they looked based on separation
 switch_df$act_acc <- switch_df$accuracy
 switch_df$act_acc[switch_df$centre == 1] <- 0.75
 
-#### make dataset for optimal accuracy ####
+#### make column for optimal accuracy ####
 # first make opt fixation location
 switch_df$opt_fix <- 0 
 switch_df$opt_fix[switch_df$separation > switch_df$switch_point] <- 1
@@ -170,8 +186,123 @@ switch_df$opt_fix[switch_df$separation > switch_df$switch_point] <- 1
 switch_df$opt_acc <- switch_df$accuracy
 switch_df$opt_acc[switch_df$opt_fix == 1] <- 0.75
 
+#### make new datasets #### 
+# first, set first half vs second half 
+switch_df$half[switch_df$block < 5] <- "first"
+switch_df$half[switch_df$block > 4] <- "second"
+
+#### Actual ####
+# sort out actual accuracy 
+temp <- group_by(switch_df, participant, half, standard_sep, condition)
+Act_accuracy <- summarise(temp, mean_acc = mean(correct))
+
+# define type of accuracy 
+Act_accuracy$acc_type <- "Actual"
+
+# change column names 
+colnames(Act_accuracy) <- c("participant",
+                            "half",
+                            "standard_sep",
+                            "condition",
+                            "Accuracy",
+                            "acc_type")
+
+
+#### Optimal ####
+# Add in optimal accuracy 
+Opt_accuracy <- summarise(temp, mean_opt = mean(opt_acc))
+
+# define type of accuracy 
+Opt_accuracy$acc_type <- "Optimal"
+
+# change column names 
+colnames(Opt_accuracy) <- c("participant",
+                            "half",
+                            "standard_sep",
+                            "condition",
+                            "Accuracy",
+                            "acc_type")
+
+#### Expected Actual ####
+# sort expected actual accuracy 
+Exp_accuracy <- summarise(temp, mean_exp = mean(act_acc))
+
+# define type of accuracy 
+Exp_accuracy$acc_type <- "Expected"
+
+# change column names 
+colnames(Exp_accuracy) <- c("participant",
+                            "half",
+                            "standard_sep",
+                            "condition",
+                            "Accuracy",
+                            "acc_type")
+
+# tidy 
+rm(temp)
+
+#### Combine these? ####
+Elle_accuracy_data <- rbind(Exp_accuracy, Opt_accuracy, Act_accuracy)
+
+# reorder 
+Elle_accuracy_data <- select(Elle_accuracy_data, 
+                             participant,
+                             condition,
+                             half,
+                             standard_sep,
+                             acc_type,
+                             Accuracy)
 
 
 
+#### reshaping data ####
+# for all the data together
+Elle_accuracy_data_wide <- dcast(Elle_accuracy_data,
+                                 participant + 
+                                   condition ~
+                                   half +
+                                   standard_sep +
+                                   acc_type,
+                                 value.var = "Accuracy")
 
+# each individually 
+# Actual
+Act_accuracy_wide <- dcast(Act_accuracy,
+                           participant + 
+                             condition ~
+                             half +
+                             standard_sep +
+                             acc_type,
+                           value.var = "Accuracy")
+
+# Optimal
+Opt_accuracy_wide <- dcast(Opt_accuracy,
+                           participant + 
+                             condition ~
+                             half +
+                             standard_sep +
+                             acc_type,
+                           value.var = "Accuracy")
+
+# Expected 
+Exp_accuracy_wide <- dcast(Exp_accuracy,
+                           participant + 
+                             condition ~
+                             half +
+                             standard_sep +
+                             acc_type,
+                           value.var = "Accuracy")
+
+# save txt files to be imported to excel 
+# Act
+write.table(Act_accuracy_wide, "Elle_data/Actual_Accuracy", row.names = FALSE, sep = "\t")
+
+# Exp
+write.table(Exp_accuracy_wide, "Elle_data/Expected_Accuracy", row.names = FALSE, sep = "\t")
+
+# Opt
+write.table(Opt_accuracy_wide, "Elle_data/Optimal_Accuracy", row.names = FALSE, sep = "\t")
+
+#### Standing pos propportions ####
+# to be done
 
