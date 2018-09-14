d9ee361bbc4d8f3ab443abe195aa479606cbcc2e
Alasdair Clarke <a.clarke@essex.ac.uk>
2018-09-14 16:54:20 +0100

    models
    still need to do 2afc

--- Instructed_Eye_Movements/4_Alasdair_analysis.R
+++ Instructed_Eye_Movements/4_Alasdair_analysis.R
@@ -1,79 +1,13 @@
-#### Alasdair's Analysis
+  #### Alasdair's Analysis
 #### libraries needed ####
 library(tidyverse)
 library(rethinking)
-rstan_options(auto_write = TRUE)
 library(viridisLite)
 
 # rethinking options 
 rstan_options(auto_write = TRUE)
 options(mc.cores = parallel::detectCores())
 
-# fit_model <- function(df) {
-#   m <- map2stan( 
-#     alist(
-#       correct ~ dbinom(1, p),
-#       logit(p) <- a + a_p[participant] + b * given_instruction,
-#       a ~ dnorm(0, 1),
-#       b ~ dnorm(0, 1),
-#       a_p[participant] ~ dnorm(0, sigma_p),
-#       sigma_p ~ dcauchy(0, 1)),
-#     data = df)
-
-#   return(m)
-
-# }
-
-# plot_indiv <- function(df, m, fname) {
-#   # extract things from model
-#   # first, we get each person's fit
-#   pred_data <- list(
-#     participant = rep(1:length(unique(df$participant)), each = 2),
-#     given_instruction =   rep(c(0, 1), length(unique(df$participant))))
-
-#   mu <- link(m, data = pred_data)
-#   mu.PI <- apply(mu, 2, PI)
-#   pred_data$lower <- mu.PI[1,]
-#   pred_data$upper <- mu.PI[2,]
-#   pred_data$condition <- ifelse(
-#     pred_data$given_instruction == 1, "Instructions", "No_instructions")
-#   pred_data <- as.data.frame(pred_data)
-
-
-#   #  now we get the fixed effects
-
-#   # plot what we got!
-
-#   acc_df <- aggregate(data = df, correct ~ participant + condition, "mean")
-
-#   plt <- ggplot(x = as.numeric(acc_df$participant))
-#   plt <- plt + geom_point(data = acc_df, aes(x = participant, y = correct, colour = condition))
-#   # add model details!
-#   plt <- plt + geom_errorbar(data = pred_data, aes(x = participant, ymin = lower, ymax = upper, colour = condition))
-#   # format nicely
-#   plt <- plt  + scale_colour_solarized()
-#   plt <- plt + scale_y_continuous(limits = c(0,1), expand = c(0,0), breaks = c(0, 0.5, 1))
-#   plt
-#   ggsave(fname)
-
-# }
-
-
-# plot_fix_ef <- function(m, fname) {
-#   # plot effect of Instructions
-#   post <- extract.samples(m)
-
-#   fx <- tibble(
-#     condition = rep(c("No_instructions", "Instructions", "Difference"), each = length(post$a)),
-#     samples = c(logistic(post$a), logistic(post$a + post$b),  logistic(post$a + post$b) - logistic(post$a)) )
-
-#   plt <- ggplot(fx, aes(x = samples, fill = condition))
-#   plt <- plt + geom_density(alpha = 0.5)
-#   plt <- plt + theme_bw()  + scale_fill_solarized()
-#   plt <- plt + scale_x_continuous(
-#     name = "probablity of correctly responding to target", limits = c(-0.2,1), expand = c(0,0))
-#   ggsave(fname)
-# }
 
 
 # # load in the data set 
@@ -87,25 +21,7 @@ df$switch_point <- as.numeric(df$switch_point)
 
 df$given_instruction <- as.numeric(df$condition == "Instructions")
 
-# # take part 2 for the main comparison
-
-
-# # fit model
-# sdf <- filter(df, block <= 4)
-# m <- fit_model(sdf)
-# plot_indiv(sdf, m, "scratch/first_four_blocks_indiv.png")
-# plot_fix_ef(m, "scratch/first__four_blocks_effect.png")
-
-
-# # fit model
-# sdf <- filter(df, block > 4)
-# m <- fit_model(sdf)
-# plot_indiv(sdf, m, "scratch/second_four_blocks_indiv.png")
-# plot_fix_ef(m, "scratch/second__four_blocks_effect.png")
-
-
-
-
+df$sep_scaled <- df$separation / max(df$separation)
 
 #### now do all sessions together ####
 df$second_half <- as.numeric(df$block > 4)
@@ -113,12 +29,15 @@ df$second_half <- as.numeric(df$block > 4)
 # save this 
 save(df, file = "scratch/models_df")
 
-#### RERUN THIS WITH NEW NAMES ####
+# not taking separation in to account
 m1 <- map2stan( 
   alist(
     correct ~ dbinom(1, p),
-    logit(p) <- a + a_p[participant] + instruction * given_instruction + half * second_half  + halfbyinst * second_half * given_instruction,
-    a ~ dnorm(0, 1),
+    mafc.logit(p) <- pmax(0.5, a + a_p[participant] +       
+      instruction * given_instruction + 
+      half * second_half + 
+      halfbyinst * second_half * given_instruction),
+    a ~ dnorm(0, 1),   
     instruction ~ dnorm(0, 1),
     half ~ dnorm(0, 1),
     halfbyinst ~ dnorm(0, 1),
@@ -127,33 +46,145 @@ m1 <- map2stan(
   data = df,
   iter = 2000, warmup = 1000, chains = 3, cores = 3)
 
+# taking separation in to account, no interactions
+m2 <- map2stan( 
+  alist(
+    correct ~ dbinom(1, p),
+    logit(p) <- 
+      a + a_p[participant] + 
+      b * sep_scaled + 
+      instruction * given_instruction + 
+      half * second_half + 
+      halfbyinst * second_half * given_instruction,
+    a ~ dnorm(0, 1),
+    b ~ dnorm(0, 1),
+    instruction ~ dnorm(0, 1),
+    half ~ dnorm(0, 1),
+    halfbyinst ~ dnorm(0, 1),
+    a_p[participant] ~ dnorm(0, sigma_p),
+    sigma_p ~ dcauchy(0, 1)),
+  data = df,
+  iter = 2000, warmup = 1000, chains = 3, cores = 3)
+
+# taking separation in to account, including interaction with practice effect
+m3 <- map2stan( 
+  alist(
+    correct ~ dbinom(1, p),
+    logit(p) <- 
+      a + a_p[participant] + 
+      b * sep_scaled + 
+      instruction * given_instruction + 
+      half * second_half + 
+      halfbysep * second_half * sep_scaled + 
+      halfbyinst * second_half * given_instruction,
+    a ~ dnorm(0, 1),
+    b ~ dnorm(0, 1),
+    instruction ~ dnorm(0, 1),
+    half ~ dnorm(0, 1),
+    halfbysep ~ dnorm(0, 1),
+    halfbyinst ~ dnorm(0, 1),
+    a_p[participant] ~ dnorm(0, sigma_p),
+    sigma_p ~ dcauchy(0, 1)),
+  data = df,
+  iter = 2000, warmup = 1000, chains = 3, cores = 3)
+
+
+
+# taking separation in to account, more interactions
+m4 <- map2stan( 
+  alist(
+    correct ~ dbinom(1, p),
+    logit(p) <- 
+      a + a_p[participant] + 
+      b * sep_scaled + 
+      instruction * given_instruction + 
+      instrbysep * given_instruction * sep_scaled + 
+      half * second_half + 
+      halfbysep * second_half * sep_scaled + 
+      halfbyinst * second_half * given_instruction,
+    a ~ dnorm(0, 1),
+    b ~ dnorm(0, 1),
+    instruction ~ dnorm(0, 1),
+    instrbysep  ~ dnorm(0, 1),
+    half ~ dnorm(0, 1),
+    halfbysep ~ dnorm(0, 1),
+    halfbyinst ~ dnorm(0, 1),
+    a_p[participant] ~ dnorm(0, sigma_p),
+    sigma_p ~ dcauchy(0, 1)),
+  data = df,
+  iter = 2000, warmup = 1000, chains = 3, cores = 3)
+
+
+
+# taking separation in to account, more interactions
+m5 <- map2stan( 
+  alist(
+    correct ~ dbinom(1, p),
+    logit(p) <- 
+      a + a_p[participant] + 
+      b * sep_scaled + 
+      instruction * given_instruction + 
+      instrbysep * given_instruction * sep_scaled + 
+      half * second_half + 
+      halfbysep * second_half * sep_scaled + 
+      halfbyinst * second_half * given_instruction + 
+      threeway * second_half * given_instruction + sep_scaled,
+    a ~ dnorm(0, 1),
+    b ~ dnorm(0, 1),
+    instruction ~ dnorm(0, 1),
+    instrbysep  ~ dnorm(0, 1),
+    half ~ dnorm(0, 1),
+    halfbysep ~ dnorm(0, 1),
+    halfbyinst ~ dnorm(0, 1),
+    threeway ~ dnorm(0, 1),
+    a_p[participant] ~ dnorm(0, sigma_p),
+    sigma_p ~ dcauchy(0, 1)),
+  data = df,
+  iter = 2000, warmup = 1000, chains = 3, cores = 3)
+
+compare(m1, m2, m3, m4, m5)
 # save this model 
-save(m1, file = "scratch/models/m1_rand_intercept")
+save(m1, m2, m3, m4, file = "scratch/models/m1_rand_intercept")
 
 
-post <- extract.samples(m1)
+post <- extract.samples(m4)
 
 # post$ etc saved as a,b,c,d for now
-fx <- tibble(
-  condition = rep(c(
-    "Baseline", 
-    "Instructed", 
-    "Practice",
-    "Transfer"), each = length(post$a)),
-  samples = c(
-    logistic(post$a), 
-    logistic(post$a + post$instruction), 
-    logistic(post$a + post$half),
-    logistic(post$a + post$instruction + post$half + post$halfbyinst)))
 
+
+get_fx_for_sep <- function(d, post) {
+  print(delta)
+  fx <- tibble(
+    condition = rep(c(
+      "Baseline", 
+      "Instructed", 
+      "Practice",
+      "Transfer"), each = length(post$a)),
+    delta = d,
+    samples = c(
+      logistic(post$a + post$b * d), 
+      logistic(post$a + post$instruction + post$b  * d + post$instrbysep * d), 
+      logistic(post$a + post$half + post$b  * d + post$halfbysep  * d),
+      logistic(post$a + post$instruction + post$half + post$halfbyinst + post$b * d + post$instrbysep * d + post$halfbysep * d)))
+
+    return(fx)
+}
+
+
+fx_min_sep  <- get_fx_for_sep(min(df$sep_scaled), post)
+fx_mean_sep <- get_fx_for_sep(mean(df$sep_scaled), post)
+fx_max_sep  <- get_fx_for_sep(max(df$sep_scaled), post)
+
+fx <- bind_rows(fx_min_sep, fx_mean_sep, fx_max_sep)
 
 plt <- ggplot(fx, aes(x = samples, fill = condition))
 plt <- plt + geom_density(alpha = 0.5)
 plt <- plt + theme_bw()  + scale_fill_viridis_d()
+plt <- plt + facet_wrap(~delta)
 plt <- plt + scale_x_continuous(
-  name = "probablity of correctly responding to target", limits = c(0.5,1), expand = c(0,0))
+  name = "probablity of correctly responding to target", limits = c(0.2,1), expand = c(0,0))
 
-ggsave("scratch/plots/joint_model.png")
+ggsave("scratch/plots/joint_model.png", width = 8, height = 3)
 
 
 #  calculate HDPI
