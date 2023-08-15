624134b4bba12db58cf79fdb60be6fca228a89f8
Alasdair Clarke <a.clarke@essex.ac.uk>
2023-08-15 14:06:56 +0100

    swithing factor labels

--- Experiment 1/plot1_bc.R
+++ Experiment 1/plot1_bc.R
@@ -18,8 +18,8 @@ post_c <- read_csv("1C/1c_post.csv")
 post_b %>% mutate(group = as_factor(group),
                   group = fct_recode(group, 
                                      "1b: reaching" = "ReachingTask",
-                                     "1b: logic puzzles" = "LogicPuzzles",
-                                     "1b: maths questions" = "MathsQuestions")) -> post_b
+                                     "1b: logic" = "LogicPuzzles",
+                                     "1b: maths" = "MathsQuestions")) -> post_b
 
 
 post_c %>% mutate(group = as_factor(group),
@@ -28,7 +28,10 @@ post_c %>% mutate(group = as_factor(group),
                                      "1c: sudoku" = "sudoku")) -> post_c
 
 post <- bind_rows(prior, post_b, post_c) %>%
-  mutate(group = as_factor(group))
+  rename(hoops = "hoop") %>%
+  mutate(group = as_factor(group),
+         hoops = as_factor(hoops), 
+         hoops = fct_relevel(hoops, "near"))
 
                   
 
@@ -37,7 +40,7 @@ post %>% filter(param == "hu") %>%
   rename(hu = ".value") %>%
   select(-param) -> post_hu
 
-ggplot(post_hu, aes(hoop, p, colour = group)) + 
+ggplot(post_hu, aes(hoops, p, colour = group)) + 
   stat_interval(alpha = 0.5, position = position_dodge(width = 0.5)) +
   scale_y_continuous("Pr(stand at central position)", 
                      limits = c(0, 1), expand = c(0,0)) +
@@ -49,10 +52,11 @@ post %>% filter(param == "b") %>%
   select(-param) -> post_b
 
 
-ggplot(post_b, aes(hoop, pos, colour = group)) + 
+ggplot(post_b, aes(hoops, pos, colour = group)) + 
   stat_interval(alpha = 0.5, position = position_dodge(width = 0.5)) +
   scale_y_continuous("normalised distance from centre", limits = c(0, 5.2), expand = c(0, 0)) +
   scale_colour_manual(values = c("#aaaaaa", "#0077bb", "#33bbee", "#009988", "#ee7733", "#cc3311")) +
   geom_hline(yintercept = 1, linetype  =2)-> plt_b
 
 plt_hu + plt_b + plot_layout(guides = "collect")
+ggsave("exp1bc_post.png", width = 8, height = 3)
