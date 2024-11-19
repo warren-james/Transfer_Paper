08983ca7dad54e82372f4b9832e3c3019e333f2f
Alasdair Clarke <a.clarke@essex.ac.uk>
2024-11-19 15:06:47 +0000

    new model

--- Instructed_Eye_Movements/2_part2_analysis.R
+++ Instructed_Eye_Movements/2_part2_analysis.R
@@ -13,78 +13,13 @@ inv_logit <- function(x) {
   return(z)
 }
 
-make_pred <- function(.draw, group, block, intercept, slope, guess) {
-  
-  sep = seq(0, 1, 0.01)
-  
-  eta = intercept + slope * sep
-  
-  p <- inv_logit(guess) + (1-inv_logit(guess)) * inv_logit(eta)
-  
-  return(tibble(.draw = .draw, 
-                group = group, block = block,
-                sep = sep, p = p))
-
-}
-
-m %>% brms::as_draws_df() %>%
-  select(-.chain, -.iteration) %>%
-  pivot_longer(-.draw, names_to = "param") %>%
-  filter(str_detect(param, "b_")) %>%
-  mutate(param = str_remove(param, "b_")) %>%
-  separate(param, c("eq", "param"), "_") -> post
-
-
-post %>% 
-  filter(eq == "guess") %>%
-  select(-eq) %>%
-  separate(param, c("group", "block"), ":") %>%
-  mutate(group = str_remove(group, "group"),
-         block = str_remove(block, "blockblk")) %>%
-  rename(guess = "value") -> post_guess
-
-post %>% 
-  filter(eq == "eta") %>%
-  select(-eq) %>%
-  mutate(var = if_else(str_detect(param, ":sep"), "slope", "intercept"),
-         param = str_remove_all(param, ":sep")) %>%
-  separate(param, c("group", "block"), ":") %>%
-  mutate(group = str_remove(group, "group"),
-         block = str_remove(block, "blockblk")) %>%
-  pivot_wider(names_from = "var") -> post_eta 
-
-
-post <- full_join(post_eta, post_guess,
-                  by = join_by(.draw, group, block))
-
-rm(post_eta, post_guess)
-
-
-post %>% pmap_df(make_pred) -> pred
-
-
-pred %>% group_by(group, block, sep) %>%
-  median_hdci(p) %>%
-  ggplot(aes(sep, p, fill = group)) + 
-  geom_ribbon(aes(ymin = .lower, ymax = .upper),
-              alpha = 0.5) +
-  geom_path(data = adat, 
-            aes(y = correct, colour = group, group = interaction(group, participant)),
-            alpha = 0.25) + 
-  geom_point(data = adat, 
-            aes(y = correct, colour = group, group = interaction(group, participant)),
-            alpha = 0.25) + 
-  geom_hline(yintercept = c(0.75, 0.5), linetype = 2) + 
-  facet_grid(. ~ block)
-
-
-ggplot(post, aes(slope, fill = group)) + geom_density() +
-  facet_grid(block ~ .)
-
-
-
 
 ####
+
+dat %>% 
+  filter(group != "simulated") %>%
+  group_by(participant, group, block, sep) %>%
+  summarise(correct = mean(correct, na.rm = TRUE)) -> adat
 
 
 dat %>% 
---
+++ Instructed_Eye_Movements/choice_data.R
@@ -0,0 +1,79 @@
+# new script to analyse choice data
+
+
+
+folder <- "data/results/Part_2-3/"
+files <- dir(folder, full.names = TRUE)
+
+# parse condition info, saved as part = 2 or 3 in filename
+condition_code <- regmatches(files, regexpr('(?<=part)[23]*', files, perl = TRUE))
+condition_code <- if_else(condition_code == "2", "no instruction", "instruction")
+
+# parse participant info
+p_id <- regmatches(files, regexpr('(?<=_)[0-9]*(?=_)', files, perl = TRUE))
+
+# read in data
+map_df(files, read_csv,
+       col_names = c("block", "sep", "fixated", "correct"),
+       show_col_types = FALSE) %>%
+  mutate(
+    participant = rep(p_id, each = 90),
+    group = as_factor(rep(condition_code, each = 90)),
+    block = if_else(block < 5, 1, 2)) %>%
+  select(participant, group, block, sep, fixated, correct) -> dat
+
+# fix block two labels
+intructed_group <- dat %>% filter(block == 1, group == "instruction") %>%
+  group_by(participant) %>% summarise()
+
+dat %>% mutate(
+  group = if_else((participant %in% intructed_group$participant) & (group != "simulated") & (block == 2),
+                  "instruction", group)) -> dat
+
+
+dat %>% mutate(fixated_side = if_else(fixated != 1, 1, 0)) -> dat
+
+# fix sep variable
+dat$sep <- scale(dat$sep)
+
+
+dat %>% group_by(participant, group, block, sep) %>%
+  summarise(fixated_side = mean(fixated_side)) -> adat
+
+adat %>%
+  ggplot(aes(sep, fixated_side, colour = group, group = participant)) +
+  geom_point() + geom_path() +
+  facet_wrap(~block)
+
+# priors!
+my_priors <- c(
+  prior(normal(0, 1), class = b))
+
+m <- brm(data = dat,
+     fixated_side ~ 0 + group:block + group:block:sep,
+     family = bernoulli(),
+     prior = my_priors,
+     chains = 4,
+     iter = 500)
+
+save(m, file = "mchoice_brms")
+
+
+dat %>% 
+  modelr::data_grid(group, block, sep = seq(0, 12.5, 0.05)) %>% 
+  add_epred_draws(m, re_formula = NA) %>% 
+  group_by(group, block, sep) %>%
+  median_hdci() %>%
+  ggplot(aes(sep, .epred, fill = group)) + 
+  geom_ribbon(aes(ymin = .lower, ymax = .upper),
+              alpha = 0.5) +
+  geom_path(data = adat, 
+            aes(y = fixated_side, colour = group, 
+                group = interaction(group, participant)),
+            alpha = 0.25) + 
+  facet_wrap( ~block) +
+  ggthemes::scale_fill_calc() +
+  ggthemes::scale_colour_calc() +
+  theme_bw()
+
+
