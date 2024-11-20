a204847c6f7df510d511dcb44cb7f4e485b6c2c6
Alasdair Clarke <a.clarke@essex.ac.uk>
2024-11-20 14:59:00 +0000

    puzzling visual degree calc

--- Instructed_Eye_Movements/choice_data.R
+++ Instructed_Eye_Movements/choice_data.R
@@ -36,6 +36,9 @@ dat %>% mutate(fixated_side = if_else(fixated != 1, 1, 0)) -> dat
 # fix sep variable
 dat$sep <- scale(dat$sep)
 
+# try making block more categorical in its labels
+dat %>% mutate(block = if_else(block == "1", "block 1", "block 2")) -> dat
+
 
 dat %>% group_by(participant, group, block, sep) %>%
   summarise(fixated_side = mean(fixated_side)) -> adat
@@ -47,33 +50,38 @@ adat %>%
 
 # priors!
 my_priors <- c(
-  prior(normal(0, 1), class = b))
+  prior(normal(0, 1), class = b),
+  prior(normal(0, 2), class = b, coef = "groupinstruction:blockblock1:sep"),
+  prior(normal(0, 2), class = b, coef = "groupnoinstruction:blockblock1:sep"),
+  prior(normal(0, 2), class = b, coef = "groupinstruction:blockblock2:sep"),
+  prior(normal(0, 2), class = b, coef = "groupnoinstruction:blockblock2:sep"),
+  prior(normal(0, 1), class = sd))
 
 m <- brm(data = dat,
-     fixated_side ~ 0 + group:block + group:block:sep,
+     fixated_side ~ 0 + group:block + group:block:sep + (0 + group:block + group:block:sep | participant),
      family = bernoulli(),
      prior = my_priors,
      chains = 4,
-     iter = 500)
+     iter = 2000)
 
 save(m, file = "mchoice_brms")
 
-
 dat %>% 
-  modelr::data_grid(group, block, sep = seq(0, 12.5, 0.05)) %>% 
+  modelr::data_grid(group, block, sep = seq(-2, 2.5, 0.05)) %>% 
   add_epred_draws(m, re_formula = NA) %>% 
   group_by(group, block, sep) %>%
-  median_hdci() %>%
+  median_hdci(.width = c(0.5, 0.95)) %>%
   ggplot(aes(sep, .epred, fill = group)) + 
-  geom_ribbon(aes(ymin = .lower, ymax = .upper),
+  geom_ribbon(aes(ymin = .lower, ymax = .upper, 
+                  group = interaction(group, .width)),
               alpha = 0.5) +
-  geom_path(data = adat, 
-            aes(y = fixated_side, colour = group, 
-                group = interaction(group, participant)),
-            alpha = 0.25) + 
+  # geom_path(data = adat, 
+  #           aes(y = fixated_side, colour = group, 
+  #               group = interaction(group, participant)),
+  #           alpha = 0.10) + 
   facet_wrap( ~block) +
-  ggthemes::scale_fill_calc() +
-  ggthemes::scale_colour_calc() +
+  ggthemes::scale_fill_ptol() +
+  ggthemes::scale_colour_ptol() +
   theme_bw()
 
 
